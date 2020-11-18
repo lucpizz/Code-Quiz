d2cd7535a045ce0ab36d08cf94da583c5608a7b9
Luca Pizzoferrato <Luca.Pizzoferrato@Outlook.com>
2020-11-18 15:02:53 -0500

    updated index.html script.js and style.css

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+const quizQuestions = [
+  {
+    question: "What is the capitol of Connecticut?",
+    answers: {
+      1: "Boston",
+      2: "Rome",
+      3: "Hartford",
+      4: "Montreal",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the capitol of Florida?",
+    answers: {
+      1: "Hartford.js",
+      2: "Boston",
+      3: "Tallahassee",
+      4: "California",
+    },
+    correctAnswer: "c",
+  },
+  {
+    question: "What is the capitol of New York?",
+    answers: {
+      1: "Paris",
+      2: "New York City",
+      3: "Los Angels",
+      4: "Tampa Bay",
+    },
+    correctAnswer: "b",
+  },
+];
+
+const startBtn = document.querySelector("#qstart-btn");
+startBtn.addEventListener("click", startQuiz);
+
+function startQuiz() {
+  console.log("Started");
+}
